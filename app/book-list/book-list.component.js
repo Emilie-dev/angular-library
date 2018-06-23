4472d86f0a9eabb345dc7dadc52ccf7c4f716bbd
Chauvin <chauvin-e@orange.fr>
2018-06-23 18:50:18 +0200

    Add a filter by categories & two way data-binding

--- app/book-list/book-list.component.js
+++ app/book-list/book-list.component.js
@@ -10,36 +10,45 @@ angular.
            author: 'J.K Rowling',
            date: '1997',
            genre: 'Fantasy',
-           image: 'img/books/harry-potter-a-lecole-des-sorciers.jpg'
+           image: 'img/books/harry-potter-a-lecole-des-sorciers.jpg',
+           age: 4
          },
          {
            title: 'Croc-Blanc',
            author: 'Jack London',
            date: '1906',
            genre: 'Aventure',
-           image: 'img/books/croc-blanc.jpg'
+           image: 'img/books/croc-blanc.jpg',
+           age: 5
          },
          {
            title: 'Détox Plus',
            author: 'Dr. Détox',
            date: '2016',
            genre: 'Diététique',
-           image: 'img/books/detox-plus.jpg'
+           image: 'img/books/detox-plus.jpg',
+           age: 2
          },
          {
            title: 'Harry Potter et la Coupe de feu',
            author: 'J.K Rowling',
            date: '2000',
            genre: 'Fantasy',
-           image: 'img/books/harry-potter-et-la-coupe-de-feu.jpg'
+           image: 'img/books/harry-potter-et-la-coupe-de-feu.jpg',
+           age: 3
          },
          {
            title: 'Apprendre AngularJS',
            author: 'A.A Angularis',
            date: '2017',
            genre: 'Informatique',
-           image: 'img/books/apprendre-angularJS.jpg'
+           image: 'img/books/apprendre-angularJS.jpg',
+           age: 1
          }
       ];
+
+      this.orderProp = 'age';
+
     }
+    
   });
