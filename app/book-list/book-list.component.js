8618ecddf9dc57c88815a5826d7d12843483b222
Chauvin <chauvin-e@orange.fr>
2018-06-26 18:39:56 +0200

    Correct books.json + Unit test BookListController - OK !

--- app/book-list/book-list.component.js
+++ app/book-list/book-list.component.js
@@ -3,51 +3,15 @@ angular.
   module('bookList').
   component('bookList', {
     templateUrl: 'book-list/book-list.template.html',
-    controller: function BookListController() {
-      this.books = [
-        {
-           title: 'Harry Potter à l\'école des sorciers',
-           author: 'J.K Rowling',
-           date: '1997',
-           genre: 'Fantasy',
-           image: './img/books/harry-potter-a-lecole-des-sorciers.jpg',
-           age: 4
-         },
-         {
-           title: 'Croc-Blanc',
-           author: 'Jack London',
-           date: '1906',
-           genre: 'Aventure',
-           image: './img/books/croc-blanc.jpg',
-           age: 5
-         },
-         {
-           title: 'Détox Plus',
-           author: 'Dr. Détox',
-           date: '2016',
-           genre: 'Diététique',
-           image: './img/books/detox-plus.jpg',
-           age: 2
-         },
-         {
-           title: 'Harry Potter et la Coupe de feu',
-           author: 'J.K Rowling',
-           date: '2000',
-           genre: 'Fantasy',
-           image: './img/books/harry-potter-et-la-coupe-de-feu.jpg',
-           age: 3
-         },
-         {
-           title: 'Apprendre AngularJS',
-           author: 'A.A Angularis',
-           date: '2017',
-           genre: 'Informatique',
-           image: './img/books/apprendre-angularJS.jpg',
-           age: 1
-         }
-      ];
+    controller: ['$http', function BookListController($http) {
+      var self = this;
+      self.orderProp = 'age';
 
-      this.orderProp = 'age';
+      $http.get('books/books.json').then(function(response) {
 
-    }
+        self.books = response.data;
+
+        });
+      }
+    ]
   });
--- app/book-list/book-list.component.spec.js
+++ app/book-list/book-list.component.spec.js
@@ -20,6 +20,19 @@ describe('bookList', function() {
       expect(ctrl.orderProp).toBe('age');
     });
 
+    var $httpBackend, ctrl;
+
+    // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+    // This allows us to inject a service and assign it to a variable with the same name
+    // as the service while avoiding a name conflict.
+    beforeEach(inject(function($componentController, _$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('books/books.json')
+                  .respond([{name: 'Croc-Blanc'}, {name: 'L\'île au trésor'}]);
+
+      ctrl = $componentController('bookList');
+    }));
+
   });
 
 });
